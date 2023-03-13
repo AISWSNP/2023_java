0b9a56d41cf4b55975d541e7c4886012fea8bc8d
Hyejeong Lee <hyejeong1019@kopo.ac.kr>
2023-03-13 17:40:54 +0900

    5일차수업내용-모두들 힘내요....

---
+++ JAVA/src/kr/ac/kopo/day05/ContinueMain.java
@@ -0,0 +1,19 @@
+package kr.ac.kopo.day05;
+
+public class ContinueMain {
+
+	public static void main(String[] args) {
+		
+		int cnt;
+		for(cnt = 1; cnt <= 10; cnt++) {
+			
+			if(cnt >= 3) {
+				continue;
+			}
+			
+			System.out.println("HI");
+		}
+		System.out.println("cnt : " + cnt);
+		
+	}
+}
